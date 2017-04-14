16b6410c02a33d959ae7e61e63bd9e10ac16c714
heaks <victor.vyshnevsky64@gmail.com>
2017-04-14 13:57:27 +0300

    lesson 117 finished

---
+++ node-chat-app/playground/time.js
@@ -0,0 +1,9 @@
+const moment = require('moment');
+
+const someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
+const createdAt = 1234;
+const date = moment(createdAt);
+console.log(date.format('h:mma'));
+
